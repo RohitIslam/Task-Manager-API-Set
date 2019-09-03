eedfded8349a684a750c56314a0b8706f568fc3b
Rohit Islam <air.rohit999@gmail.com>
2019-09-03 14:02:26 +0000

    MongoDb insert test through node js

--- mongodb.js
+++ mongodb.js
@@ -12,6 +12,65 @@ MongoClient.connect(
       return console.log("Unable to connect", error);
     }
 
-    console.log("DB Connected");
+    const db = client.db(databaseName);
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Rohit",
+    //     age: 24
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert", error);
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "John",
+    //       age: 26
+    //     },
+    //     {
+    //       name: "Doe",
+    //       age: 22
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert", error);
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          task: "Task 1",
+          description: "Have to complete tast 1",
+          completed: false
+        },
+        {
+          task: "Task 2",
+          description: "Have to complete tast 2",
+          completed: true
+        },
+        {
+          task: "Task 3",
+          description: "Have to complete tast 3",
+          completed: true
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert", error);
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
