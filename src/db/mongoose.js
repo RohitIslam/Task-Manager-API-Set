cc568a6d2efcb63d218fa38ea85b612dcbfe0089
Rohit Islam <air.rohit999@gmail.com>
2019-09-05 05:12:45 +0000

    data validation and sanitization practice through mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,35 +8,65 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot contain 'password' as a password");
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
 });
 
-const user = new User({
-  name: "Rohit",
-  age: 24
-});
+// const user = new User({
+//   name: "Robin",
+//   email: "robin@robin.com",
+//   password: "123456"
+// });
 
-user
-  .save()
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+// user
+//   .save()
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
 
 const Task = mongoose.model("Task", {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
 const task = new Task({
-  description: "Water the tree",
-  completed: true
+  description: "          Fill the jug             "
 });
 
 task
