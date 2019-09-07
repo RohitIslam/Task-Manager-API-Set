5eb4f04b2e273cd8997ba577f166c0e98e2a5f20
Rohit Islam <air.rohit999@gmail.com>
2019-09-07 05:15:35 +0000

    Api created to get all users and tasks, and get user and task by id

--- src/routes/api/tasks.js
+++ src/routes/api/tasks.js
@@ -4,6 +4,38 @@ const router = express.Router();
 //Load models
 const Task = require("../../models/Task");
 
+// GET API START
+
+// @route GET api/tasks/
+// @description get all tasks
+// @access Public
+router.get("/", (req, res) => {
+  Task.find()
+    .then(response => {
+      if (!response) {
+        return res.status(404).send("No task found");
+      }
+      res.json(response);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// @route GET api/tasks/:id
+// @description get task by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then(response => {
+      if (!response) {
+        return res.status(404).send("No task found");
+      }
+      res.json(response);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// GET API END
+
 // POST API START
 
 // @route POST api/tasks/
@@ -14,7 +46,7 @@ router.post("/", (req, res) => {
 
   task
     .save()
-    .then(() => res.send(task))
+    .then(() => res.status(201).send(task))
     .catch(err => {
       res.status(400).send(err);
     });
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,6 +4,38 @@ const router = express.Router();
 //Load models
 const User = require("../../models/User");
 
+// GET API START
+
+// @route GET api/users/
+// @description get all users
+// @access Public
+router.get("/", (req, res) => {
+  User.find()
+    .then(response => {
+      if (!response) {
+        return res.status(404).send("No user found");
+      }
+      res.json(response);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// @route GET api/users/:id
+// @description get user by id
+// @access Public
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then(response => {
+      if (!response) {
+        return res.status(404).send("No user found");
+      }
+      res.json(response);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// GET API END
+
 // POST API START
 
 // @route POST api/users/
@@ -14,10 +46,12 @@ router.post("/", (req, res) => {
 
   user
     .save()
-    .then(() => res.send(user))
+    .then(() => res.status(201).send(user))
     .catch(err => {
       res.status(400).send(err);
     });
 });
+
+// POST API END
 
 module.exports = router;
