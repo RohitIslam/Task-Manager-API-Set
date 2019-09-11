6709b419e40d9746776bdf7e1f0906e87d6dacc6
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 13:50:58 +0000

    Test case created for not deleting other user's task

--- tests/task.test.js
+++ tests/task.test.js
@@ -29,7 +29,7 @@ test("Should get all tasks for user one", async () => {
   expect(response.body.length).toBe(2);
 });
 
-// TEST CASE creating tasks for user
+// TEST CASE for creating tasks for user
 test("Should create tasks for user", async () => {
   const response = await supertest(app)
     .post("/api/tasks/")
@@ -40,5 +40,17 @@ test("Should create tasks for user", async () => {
     .expect(201);
 
   const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+});
+
+// TEST CASE for not deleting other user's task
+test("Should not delete other user's task", async () => {
+  await supertest(app)
+    .delete(`/api/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`) // setting up the Authorization header with JWT
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
   expect(task).not.toBeNull();
 });
