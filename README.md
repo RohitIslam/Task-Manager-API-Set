46d7d142e305f527adcc60f34202c2233e36a4d7
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 07:29:42 +0000

    Created basic test cases to signup and login. Jest lifcycle used

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Task-Manager
 
-API set created for task manager project using Node Js, Express Js and MongoDB
+API set created for task manager project using Node Js, Express Js and MongoDB and tested with JEST
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,45 @@
 const request = require("supertest");
+const app = require("../app");
+const User = require("../src/models/User");
+
+const userOne = {
+  name: "Test User One",
+  email: "testone@test.com",
+  password: "123456789"
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/api/users/")
+    .send({
+      name: "Test User",
+      email: "test@test.com",
+      password: "123456789"
+    })
+    .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/api/users/login")
+    .send({
+      email: "testone@test.com",
+      password: "123456789"
+    })
+    .expect(200);
+});
+
+test("Should not login non-existing user", async () => {
+  await request(app)
+    .post("/api/users/login")
+    .send({
+      email: "test@test.com",
+      password: "12345"
+    })
+    .expect(400);
+});
