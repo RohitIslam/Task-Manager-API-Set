11a40ee0ecfddfea0f61d800f5f14865369661b9
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 13:36:08 +0000

    Test case created for getting all tasks for user one

--- tests/fixtures/testDB.js
+++ tests/fixtures/testDB.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("../../src/models/User");
+const Task = require("../../src/models/Task");
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -15,13 +16,55 @@ const userOne = {
   ]
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "2nd Test User",
+  email: "testtwo@test.com",
+  password: "123456789",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Make a tea",
+  completed: false,
+  owner: userOne._id
+};
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Feed the birds",
+  completed: true,
+  owner: userOne._id
+};
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Play Games",
+  completed: false,
+  owner: userTwo._id
+};
+
 const setupTestDB = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
   await new User(userOne).save();
+  await new User(userTwo).save();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
   userOneId,
+  userTwoId,
   userOne,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setupTestDB
 };
--- tests/task.test.js
+++ tests/task.test.js
@@ -2,13 +2,32 @@ const supertest = require("supertest");
 const app = require("../app");
 const User = require("../src/models/User");
 const Task = require("../src/models/Task");
-const { userOneId, userOne, setupTestDB } = require("./fixtures/testDB");
+const {
+  userOneId,
+  userTwoId,
+  userOne,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+  setupTestDB
+} = require("./fixtures/testDB");
 
 // Dummy user setup for test cases START
 
 beforeEach(setupTestDB);
 
 // Dummy user setup for test cases END
+
+// TEST CASE for getting all tasks for user one
+test("Should get all tasks for user one", async () => {
+  const response = await supertest(app)
+    .get("/api/tasks/")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`) // setting up the Authorization header with JWT
+    .send()
+    .expect(200);
+  expect(response.body.length).toBe(2);
+});
 
 // TEST CASE creating tasks for user
 test("Should create tasks for user", async () => {
